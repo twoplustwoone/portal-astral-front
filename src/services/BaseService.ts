4b50e743850a2eca3b78fcef88ee26020af2554e
yankee <francisco.di@ing.austral.edu.ar>
2018-09-09 15:00:17 -0300

    Add `BaseService.ts`

---
+++ src/services/BaseService.ts
@@ -0,0 +1,30 @@
+export default class BaseService {
+  protected _store;
+
+  gotStore() {
+    // this is just an overideable placeholder
+  }
+
+  initializeStore(store) {
+    this._store = store;
+    this.gotStore();
+  }
+
+  dispatch = (action) => {
+    if (!this._store) {
+      console.error('[BaseService#dispatch] Store was not initialized on the service', this);
+      return Promise.reject(null);
+    }
+
+    return this._store.dispatch(action);
+  }
+
+  getState = () => {
+    if (!this._store) {
+      console.error('[BaseService.state] Store was not initialized on the service', this);
+      return undefined;
+    }
+
+    return this._store.getState();
+  }
+}
