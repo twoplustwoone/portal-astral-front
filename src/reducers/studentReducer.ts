9bb430e2901f7e32496e93a9620ab83b43802faa
yankee <francisco.di@ing.austral.edu.ar>
2018-09-18 23:02:35 -0300

    Fix delete student

--- src/reducers/studentReducer.ts
+++ src/reducers/studentReducer.ts
@@ -2,34 +2,37 @@ import { IAction, IStudent } from "../../globals";
 import { studentActions } from "../actions";
 
 export interface IState {
-    [id: string]: IStudent;
+  [id: string]: IStudent;
 }
 
 const initialState: IState = {};
 
 const reducer = (state: IState = initialState, action: IAction): IState => {
-    // @ts-ignore
-    const { type, payload } = action;
+  // @ts-ignore
+  const { type, payload } = action;
 
-    const { FETCH_STUDENTS_SUCCESS, FETCH_STUDENT_SUCCESS } = studentActions;
+  const { FETCH_STUDENTS_SUCCESS, FETCH_STUDENT_SUCCESS, DELETE_STUDENT_SUCCESS } = studentActions;
 
-    switch (type) {
+  switch (type) {
 
-        case FETCH_STUDENTS_SUCCESS:
-            return {
-                ...state,
-                ...payload.students.reduce((acc, student) => ({ ...acc, [student.id]: student }), {}),
-            };
+    case DELETE_STUDENT_SUCCESS:
+      const { [payload.professorId]: deleted, ...rest } = state;
+      return rest;
 
-        case FETCH_STUDENT_SUCCESS:
-            return {
-                ...state,
-                [payload.student.id]: payload.student,
-            };
+    case FETCH_STUDENTS_SUCCESS:
+      return {
+        ...payload.students.reduce((acc, student) => ({ ...acc, [student.id]: student }), {}),
+      };
 
-        default:
-            return state;
-    }
+    case FETCH_STUDENT_SUCCESS:
+      return {
+        ...state,
+        [payload.student.id]: payload.student,
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default reducer;
