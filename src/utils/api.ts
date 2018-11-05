b0c87a8c9b72ca42bc35926a22dbf09f8bc51d34
duraznito <sebastian.belaustegui@cloudgaia.com>
2018-11-05 16:53:59 -0300

    bug fix

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -290,7 +290,7 @@ export const createExam = (exam: IExam): Promise<Response> => {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(exam),
+        body: JSON.stringify({courseID: exam.course.id, dateTime: exam.date}),
     };
 
     return fetch(url, init);
@@ -312,7 +312,7 @@ export const updateExam = (exam: IExam): Promise<Response> => {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(exam),
+        body: JSON.stringify({courseID: exam.course.id, dateTime: exam.date}),
     };
 
     return fetch(url, init);
